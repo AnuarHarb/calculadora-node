022349068696425c7357c07f3c4ef007b1dccdd9
Anuar Harb <anuarharb@hotmail.com>
2019-02-08 20:52:41 -0600

    Calculadora con suma, resta, division y multiplicacion

---
+++ index.js
@@ -0,0 +1,6 @@
+const math = require('./math');
+
+console.log(math.suma(10, 20));
+console.log(math.resta(10, 20));
+console.log(math.multiplicacion(10, 20));
+console.log(math.divicion(10, 20));
---
+++ math.js
@@ -0,0 +1,25 @@
+module.exports = {
+  suma: function (a, b) {
+    return a + b;
+  },
+  resta: function(a, b) {
+    return a - b;
+  },
+  multiplicacion: function(a, b) {
+    return a * b;
+  },
+  divicion: function(a, b) {
+    return a / b;
+  }
+}
+
+// Ejemplo de un Objeto
+// const persona = {
+//   nombre: "anuar", //Propiedad
+//   saludar: function() { //Metodo
+//     console.log('hola');
+//   },
+//   despedirse: function() {
+//     console.log('adios');
+//   }
+// }
